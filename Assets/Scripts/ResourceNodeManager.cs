a80e0b97962f4b1fb7c6cba819a0da119ca12c58
Nolan Lee <nolanslee23@gmail.com>
2026-05-07 17:45:15 -0700

    Lowered level threshod

--- Assets/Scripts/ResourceNodeManager.cs
+++ Assets/Scripts/ResourceNodeManager.cs
@@ -238,8 +238,8 @@ public class ResourceNodeManager : MonoBehaviour
         resources.Clear();
 
         resources.Add(new ResourceData(ItemID.OakWood, "Oak Tree", ActiveSkill.Woodcutting, 1, 1, 5, 15, 25));
-        resources.Add(new ResourceData(ItemID.PineWood, "Pine Tree", ActiveSkill.Woodcutting, 2, 20, 15, 50, 100));
-        resources.Add(new ResourceData(ItemID.ElmWood, "Elm Tree", ActiveSkill.Woodcutting, 3, 40, 40, 150, 500));
-        resources.Add(new ResourceData(ItemID.AspenWood, "Aspen Tree", ActiveSkill.Woodcutting, 4, 60, 60, 1000, 1000));
+        resources.Add(new ResourceData(ItemID.PineWood, "Pine Tree", ActiveSkill.Woodcutting, 2, 5, 15, 50, 100));
+        resources.Add(new ResourceData(ItemID.ElmWood, "Elm Tree", ActiveSkill.Woodcutting, 3, 10, 40, 150, 500));
+        resources.Add(new ResourceData(ItemID.AspenWood, "Aspen Tree", ActiveSkill.Woodcutting, 4, 20, 60, 1000, 1000));
     }
 }
